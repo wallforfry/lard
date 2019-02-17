a33f615546c5b2b1931396e143dbfc936a9f1e8e
Wallerand Delevacq <wallforfry@gmail.com>
2019-02-17 11:55:05 +0100

    Add base tree

---
+++ lard_website/api/urls.py
@@ -0,0 +1,14 @@
+"""
+Project : lard
+File : urls
+Author : DELEVACQ Wallerand
+Date : 17/02/19
+"""
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index)
+]
---
+++ lard_website/front/urls.py
@@ -0,0 +1,15 @@
+"""
+Project : lard
+File : urls
+Author : DELEVACQ Wallerand
+Date : 17/02/19
+"""
+
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index)
+]
