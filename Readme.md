ad0bcc6ceda16909a092a057f97817290a537280
Wallerand Delevacq <wallforfry@gmail.com>
2019-02-17 21:59:54 +0100

    Add docker support

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Lard
+
+## Requirements
+- var.env
+    - RECAPTCHA_SITE_KEY
+    - RECAPTCHA_SECRET_KEY
--- lard_website/lard_website/settings.py
+++ lard_website/lard_website/settings.py
@@ -84,9 +84,17 @@ WSGI_APPLICATION = 'lard_website.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lard',
+        'USER': 'lard',
+        'PASSWORD': 'dral',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
