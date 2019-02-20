2aeb839a52697762eae50f68520d20f5846c3cbe
Alusio <contact@alusio.fr>
2019-02-20 20:01:47 +0100

    Add a menu on the node

--- lard_website/front/static/js/Cytoscape/main.js
+++ lard_website/front/static/js/Cytoscape/main.js
@@ -1,85 +1,85 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     var cy = window.cy = cytoscape({
 
-        container: document.getElementById('cy'),
+        container: document.getElementById("cy"),
 
 
         layout: {
-            name: 'grid',
+            name: "grid",
             rows: 2,
             cols: 2
         },
 
         style: [
             {
-                selector: 'node',
+                selector: "node",
                 style: {
-                    'content': 'data(name)',
-                    'shape': 'square',
+                    "content": "data(name)",
+                    "shape": "square",
                 }
             },
 
             {
-                selector: 'edge',
+                selector: "edge",
                 style: {
-                    'curve-style': 'bezier',
-                    'content': 'data(name)',
-                    'target-arrow-shape': 'triangle'
+                    "curve-style": "bezier",
+                    "content": "data(name)",
+                    "target-arrow-shape": "triangle"
                 }
             },
 
             // some style for the extension
 
             {
-                selector: '.eh-handle',
+                selector: ".eh-handle",
                 style: {
-                    'background-color': 'red',
-                    'width': 12,
-                    'height': 12,
-                    'shape': 'ellipse',
-                    'overlay-opacity': 0,
-                    'border-width': 12, // makes the handle easier to hit
-                    'border-opacity': 0
+                    "background-color": "red",
+                    "width": 12,
+                    "height": 12,
+                    "shape": "ellipse",
+                    "overlay-opacity": 0,
+                    "border-width": 12, // makes the handle easier to hit
+                    "border-opacity": 0
                 }
             },
 
             {
-                selector: '.eh-hover',
+                selector: ".eh-hover",
                 style: {
-                    'background-color': 'red'
+                    "background-color": "red"
                 }
             },
 
             {
-                selector: '.eh-source',
+                selector: ".eh-source",
                 style: {
-                    'border-width': 2,
-                    'border-color': 'red'
+                    "border-width": 2,
+                    "border-color": "red"
                 }
             },
 
             {
-                selector: '.eh-target',
+                selector: ".eh-target",
                 style: {
-                    'border-width': 2,
-                    'border-color': 'red'
+                    "border-width": 2,
+                    "border-color": "red"
                 }
             },
 
             {
-                selector: '.eh-preview, .eh-ghost-edge',
+                selector: ".eh-preview, .eh-ghost-edge",
                 style: {
-                    'background-color': 'red',
-                    'line-color': 'red',
-                    'target-arrow-color': 'red',
-                    'source-arrow-color': 'red'
+                    "background-color": "red",
+                    "line-color": "red",
+                    "target-arrow-color": "red",
+                    "source-arrow-color": "red"
                 }
             },
 
             {
-                selector: '.eh-ghost-edge.eh-preview-active',
+                selector: ".eh-ghost-edge.eh-preview-active",
                 style: {
-                    'opacity': 0
+                    "opacity": 0
                 }
             }
         ],
@@ -113,117 +113,115 @@ document.addEventListener('DOMContentLoaded', function () {
     let defaults = {
         preview: true, // whether to show added edges preview before releasing selection
         hoverDelay: 150, // time spent hovering over a target node before it is considered selected
-        handleNodes: 'node', // selector/filter function for whether edges can be made from a given node
+        handleNodes: "node", // selector/filter function for whether edges can be made from a given node
         snap: false, // when enabled, the edge can be drawn by just moving close to a target node (can be confusing on compound graphs)
         snapThreshold: 50, // the target node must be less than or equal to this many pixels away from the cursor/finger
         snapFrequency: 15, // the number of times per second (Hz) that snap checks done (lower is less expensive)
         noEdgeEventsInDraw: false, // set events:no to edges during draws, prevents mouseouts on compounds
         disableBrowserGestures: true, // during an edge drawing gesture, disable browser gestures such as two-finger trackpad swipe and pinch-to-zoom
-        handlePosition: function (node) {
-            return 'middle top'; // sets the position of the handle in the format of "X-AXIS Y-AXIS" such as "left top", "middle top"
-        },
-        handleInDrawMode: false, // whether to show the handle in draw mode
-        edgeType: function (sourceNode, targetNode) {
-            // can return 'flat' for flat edges between nodes or 'node' for intermediate node between them
-            // returning null/undefined means an edge can't be added between the two nodes
-            return 'flat';
-        },
-        loopAllowed: function (node) {
-            // for the specified node, return whether edges from itself to itself are allowed
-            return false;
-        },
-        nodeLoopOffset: -50, // offset for edgeType: 'node' loops
-        nodeParams: function (sourceNode, targetNode) {
-            // for edges between the specified source and target
-            // return element object to be passed to cy.add() for intermediary node
-            return {};
-        },
-        edgeParams: function (sourceNode, targetNode, i) {
-            // for edges between the specified source and target
-            // return element object to be passed to cy.add() for edge
-            // NB: i indicates edge index in case of edgeType: 'node'
-            return {};
-        },
+
         ghostEdgeParams: function () {
             // return element object to be passed to cy.add() for the ghost edge
             // (default classes are always added for you)
             return {};
         },
-        show: function (sourceNode) {
-            // fired when handle is shown
-        },
-        hide: function (sourceNode) {
-            // fired when the handle is hidden
-        },
-        start: function (sourceNode) {
-            // fired when edgehandles interaction starts (drag on handle)
-        },
-        complete: function (sourceNode, targetNode, addedEles) {
-            // fired when edgehandles is done and elements are added
-        },
-        stop: function (sourceNode) {
-            // fired when edgehandles interaction is stopped (either complete with added edges or incomplete)
-        },
-        cancel: function (sourceNode, cancelledTargets) {
-            // fired when edgehandles are cancelled (incomplete gesture)
-        },
-        hoverover: function (sourceNode, targetNode) {
-            // fired when a target is hovered
-        },
-        hoverout: function (sourceNode, targetNode) {
-            // fired when a target isn't hovered anymore
-        },
-        previewon: function (sourceNode, targetNode, previewEles) {
-            // fired when preview is shown
-        },
-        previewoff: function (sourceNode, targetNode, previewEles) {
-            // fired when preview is hidden
-        },
-        drawon: function () {
-            // fired when draw mode enabled
-        },
-        drawoff: function () {
-            // fired when draw mode disabled
-        }
+
     };
 
     var eh = cy.edgehandles(defaults);
+    let menu = cy.cxtmenu(defaults);
 
-    //Permet de delete nodes et edges avec bouton "Remove"
-    document.querySelector('#clear').addEventListener('click', function () {
-        cy.remove('node:selected');
-        cy.remove('edge:selected');
+    cy.cxtmenu({
+        selector: 'node, edge',
+
+        commands: [
+            {
+                content: 'TEST',
+                select: function (ele) {
+                    cy.remove(ele);
+                }
+            },
+
+            {
+                content: '<span class="fa fa-star fa-2x" ></span>',
+                select: function (ele) {
+                    console.log(ele.data('name'));
+                },
+                enabled: false
+            },
+
+            {
+                content: '<span class="fa fa-remove fa-2x" style="color: #c53a3a;"></span>',
+                select: function (ele) {
+                    cy.remove(ele);
+                },
+                activeFillColor: 'rgba(255,0,0,0.2)',
+            }
+        ]
     });
 
-    document.querySelector('#create').addEventListener('click', function () {
-        cy.add({
-            group: 'nodes',
-            data: {
-                "id": "AUTRE",
-                "name": "AUTRE"
+    cy.cxtmenu({
+        selector: 'core',
+
+        commands: [
+            {
+                content: 'bg1',
+                select: function () {
+                    console.log('bg1');
+                }
             },
-            position: {x: cy.width()/2, y: cy.height()/3}
+
+            {
+                content: 'bg2',
+                select: function () {
+                    console.log('bg2');
+                }
+            }
+        ]
+    });
+    var num = 1;
+
+    //Permet de delete nodes et edges avec bouton "Remove"
+    document.querySelector("#clear").addEventListener("click", function () {
+        cy.remove("node:selected");
+        cy.remove("edge:selected");
+    });
+
+    document.querySelector("#create").addEventListener("click", function () {
+
+        cy.add({
+            group: "nodes",
+            data: {
+                "id": "AUTRE_" + num,
+                "name": "AUTRE " + num
+            },
+            position: {
+                x: Math.floor(Math.random() * (cy.width() - 500)) + 300,
+                y: Math.floor(Math.random() * (cy.height() - 500)) + 100
+            }
         });
 
+
+        cy.pan();
         cy.center();
+        num++;
+        console.log(cy.width());
     });
 
 
     //Permet de delete nodes et edges avec "Suppr"
-    document.addEventListener('keydown', function (e) {
+    document.addEventListener("keydown", function (e) {
         if (e.keyCode === 46) {
-            cy.remove('node:selected');
-            cy.remove('edge:selected');
+            cy.remove("node:selected");
+            cy.remove("edge:selected");
         }
     }, false);
 
-    //Event lors de la fin de la création d'un edge
-    cy.on('ehcomplete', (event, sourceNode, targetNode, addedEles) => {
+    //Event lors de la fin de la création d"un edge
+    cy.on("ehcomplete", (event, sourceNode, targetNode) => {
         console.log("Edge : " + sourceNode.id() + " à " + targetNode.id());
 
     });
-
-    cy.resize();
 
 
 });
