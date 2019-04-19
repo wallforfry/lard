245fbac7368f02cc4f9e7e4df5c4c2a54e2efc98
Wallerand Delevacq <wallforfry@gmail.com>
2019-04-19 14:50:25 +0200

    Add blocks list in api

--- lard_website/api/urls.py
+++ lard_website/api/urls.py
@@ -10,5 +10,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('blocks/list', views.list_blocks)
 ]
--- lard_website/api/views.py
+++ lard_website/api/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from front.models import Block
+
+
 def index(request):
     return HttpResponse("Api")
+
+def list_blocks(request):
+    return HttpResponse(serializers.serialize('json', Block.objects.all()))
