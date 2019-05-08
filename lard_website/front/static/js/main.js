b5083af5fffda7a57c95161c8b21c7797ea2afdc
Wallerand Delevacq <wallforfry@gmail.com>
2019-05-08 17:14:00 +0200

    Correct cytoscape css

--- lard_website/front/static/js/main.js
+++ lard_website/front/static/js/main.js
@@ -1,38 +1,49 @@
 $.noConflict();
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-	"use strict";
+    "use strict";
 
-	[].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {
-		new SelectFx(el);
-	} );
+    [].slice.call(document.querySelectorAll('select.cs-select')).forEach(function (el) {
+        new SelectFx(el);
+    });
 
-	jQuery('.selectpicker').selectpicker;
+    jQuery('.selectpicker').selectpicker;
 
 
-	$('#menuToggle').on('click', function(event) {
-		$('body').toggleClass('open');
-	});
+    $('#menuToggle').on('click', function (event) {
+        $('body').toggleClass('open');
+    });
 
-	$('.search-trigger').on('click', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-		$('.search-trigger').parent('.header-left').addClass('open');
-	});
+    $('.search-trigger').on('click', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.search-trigger').parent('.header-left').addClass('open');
+    });
 
-	$('.search-close').on('click', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-		$('.search-trigger').parent('.header-left').removeClass('open');
-	});
+    $('.search-close').on('click', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.search-trigger').parent('.header-left').removeClass('open');
+    });
 
-	// $('.user-area> a').on('click', function(event) {
-	// 	event.preventDefault();
-	// 	event.stopPropagation();
-	// 	$('.user-menu').parent().removeClass('open');
-	// 	$('.user-menu').parent().toggleClass('open');
-	// });
+    // $('.user-area> a').on('click', function(event) {
+    // 	event.preventDefault();
+    // 	event.stopPropagation();
+    // 	$('.user-menu').parent().removeClass('open');
+    // 	$('.user-menu').parent().toggleClass('open');
+    // });
+
+    jQuery("#cy").height(jQuery("#right-panel").height() - 186);
+
+    $(window).on('resize', function () {
+        var win = $(this); //this = window
+        if (win.height() >= 820) { /* ... */
+        }
+        if (win.width() >= 1280) { /* ... */
+        }
+        jQuery("#cy").height(jQuery("#right-panel").height() - 186);
+    });
 
 
 });
