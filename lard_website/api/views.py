2ee3bb5dffe5b924195cd6297526d7306f3f9fbe
Wallerand Delevacq <wallforfry@gmail.com>
2019-05-11 18:42:42 +0200

    Correct notifications

--- lard_website/api/views.py
+++ lard_website/api/views.py
@@ -73,7 +73,7 @@ def update_result(request, worker_id):
                                "url": str(reverse("pipeline_results", kwargs={"id": r.id})), "worker_id": worker_id}))
         else:
             m.send(json.dumps({"type": "success", "title": "Pipeline terminé : ",
-                               "message": "Le pipeline " + r.pipeline.name + " s'est correctement terminé. Cliquez ici pour voir le résultat",
+                               "message": "Le pipeline <b>" + r.pipeline.name + "</b> s'est correctement terminé. Cliquez ici pour voir le résultat",
                                "url": str(reverse("pipeline_results", kwargs={"id": r.id})), "worker_id": worker_id}))
 
         return HttpResponse(status=200)
