a0704e865461e8130ff122f0a9aaef37b8441f7f
Wallerand Delevacq <wallforfry@gmail.com>
2019-05-12 15:10:13 +0200

    Update PipelineResult Admin view

--- lard_website/front/admin.py
+++ lard_website/front/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from front.models import Pipeline, Block, InputOutputType, InputOutput, PipelineResult
 
+class PipelineResultAdmin(admin.ModelAdmin):
+    search_fields = ('pipeline', 'user', 'created_at', 'updated_at')
+    list_display = ('pipeline', 'user', 'created_at', 'updated_at', 'worker_id')
+    list_filter = ('user', 'pipeline')
+
+
 admin.site.register(Pipeline)
-admin.site.register(PipelineResult)
+admin.site.register(PipelineResult, PipelineResultAdmin)
 admin.site.register(Block)
 admin.site.register(InputOutput)
 admin.site.register(InputOutputType)
